703cc3f5a37e7b8a22ca6ef14bfa4394ebdc3da7
MD.TORIKUL ISLAM <75210979+TorikuIislam@users.noreply.github.com>
2022-06-25 15:30:53 +0600

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,11 +2,11 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "form_regi";
+$dbname = "loginpage";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+  die("Connection failed: " .mysqli_connect_error());
 }
