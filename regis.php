75d5dbd0703d94bbfd8a6be7c154ab5a9d70d2d7
MD.TORIKUL ISLAM <75210979+TorikuIislam@users.noreply.github.com>
2022-06-25 14:57:27 +0600

    Add files via upload

---
+++ regis.php
@@ -0,0 +1,26 @@
+<?php
+
+include('connection.php');
+if(isset($_POST['submit']))
+
+
+{
+
+  $name=$_POST['fname'];
+  $email=$_POST['email'];
+  $password=$_POST['pin'];
+
+  $sql = "insert into registration (NAME, EMAIL, PASSWORD)
+  VALUES ('$name', '$email', '$password')";
+  $data=mysqli_query($conn,$sql);
+
+  if($data){
+
+    echo" Data succesfully entry";
+
+  }
+  else{
+    echo"failed to entry";
+  }
+} 
+?>
